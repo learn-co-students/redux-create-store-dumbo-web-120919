52e54799ba2590d9e0bdb07202fe255ad0e33dc6
ShaquilleClarke <github email address>
2020-03-08 21:09:25 -0400

    Done.

--- js/createStore.js
+++ js/createStore.js
@@ -1,28 +1,53 @@
-let state;
+
+
+function createStore() {
+  let state;
+
+  function dispatch(action){
+    state = reducer(state, action);
+    render();
+  };
+
+  function getState() {
+    return state;
+  }
+
+  return { dispatch, getState };
+}
 
 function reducer(state = { count: 0 }, action) {
   switch (action.type) {
     case 'INCREASE_COUNT':
       return { count: state.count + 1 };
+    
+    case 'DECREASE_COUNT':
+      return { count: state.count - 1};
 
     default:
       return state;
   }
 };
 
-function dispatch(action){
-  state = reducer(state, action);
-  render();
-};
+let store = createStore();
+store.dispatch({type: '@@INIT'});
+  
+
 
 function render() {
   let container = document.getElementById('container');
-  container.textContent = state.count;
+  container.textContent = store.getState().count;
 };
 
-dispatch({ type: '@@INIT' })
-let button = document.getElementById('button');
+store.dispatch({ type: '@@INIT' })
 
-button.addEventListener('click', function() {
-    dispatch({ type: 'INCREASE_COUNT' });
+let button = document.getElementById('button');
+button.addEventListener('click', () => {
+    store.dispatch({ type: 'INCREASE_COUNT' });
 })
+
+let button2 = document.querySelector('#button2')
+button2.addEventListener('click', () => {
+  store.dispatch({type: 'DECREASE_COUNT'})
+})
+
+
